1299176c8258817b56e0a6d15fed146e1455f9b4
luke0408 <zjvl324@gmail.com>
2026-03-23 11:57:46 +0900

    chore: update prisma defaults to phos_dev schema

--- apps/api/src/modules/prisma/prisma.service.ts
+++ apps/api/src/modules/prisma/prisma.service.ts
@@ -2,7 +2,7 @@ import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@phos/db';
 
-const DEFAULT_DATABASE_URL = 'postgresql://phos:phos@localhost:5432/phos?schema=public';
+const DEFAULT_DATABASE_URL = 'postgresql://phos:phos@localhost:5432/phos?schema=phos_dev';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleDestroy {
