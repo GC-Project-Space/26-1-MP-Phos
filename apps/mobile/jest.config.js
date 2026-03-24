e2c911e863be89d066b7b8b23ce101ee80f80a9a
luke0408 <zjvl324@gmail.com>
2026-03-24 22:32:32 +0900

    test(test): stabilize mobile jest and rntl setup
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.[jt]sx?$': ['babel-jest', { presets: ['babel-preset-expo'] }],
